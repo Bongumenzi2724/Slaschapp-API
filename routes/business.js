25f7e9da5a059638d74d9f37ec6b9111319abae8
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-02-15 15:00:50 +0200

    "Auction Controller Update"

--- routes/business.js
+++ routes/business.js
@@ -3,8 +3,7 @@ const express=require('express')
 const router=express.Router()
 
 const {createBusiness,deleteBusiness,getAllBusinesses,updateBusinessDetails,getSingleBusiness}=require('../controllers/business')
-const {createAuction}=require('../controllers/auction')
-
+const {createAuction} = require('../controllers/auction')
 router.route('/').post(createBusiness).get(getAllBusinesses)
 router.route('/:id').get(getSingleBusiness).delete(deleteBusiness).patch(updateBusinessDetails)
 router.route('/auction').post(createAuction)
