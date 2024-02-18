b68f1ebc8e5c489fe1aa9330068dc65230c66415
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-02-18 18:04:37 +0200

    "Updated Swagger Documentation"

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ const options={
 } 
 
 app.get('/',(req,res)=>{
-    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+    res.send('<h1>Business API</h1><a href="/api-docs">Documentation</a>');
 })
 
 //const specs=swaggerJSdoc(options)
@@ -64,6 +64,7 @@ app.get('/',(req,res)=>{
 app.use("/api-docs",swaggerUI.serve,swaggerUI.setup(swaggerDocument)) 
 
 const port=process.env.PORT||5000
+
 const start=async()=>{
     try{
         await connectDB(process.env.MONGO_URL)
