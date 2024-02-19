68377390bb1eb3eb6ee8cf51f8579579d25b16c1
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-02-19 15:46:10 +0200

    "Simple Alignment"

--- routes/business.js
+++ routes/business.js
@@ -2,8 +2,10 @@ const express=require('express')
 const router=express.Router()
 const {createBusiness,deleteBusiness,getAllBusinesses,updateBusinessDetails,getSingleBusiness}=require('../controllers/business')
 const {createAuction, auctionSearchResults} = require('../controllers/auction')
+
 router.route('/').post(createBusiness).get(getAllBusinesses)
 router.route('/:id').get(getSingleBusiness).delete(deleteBusiness).patch(updateBusinessDetails)
 router.route('/auction').post(createAuction)
 router.route('/auction/:key').get(auctionSearchResults)
+
 module.exports=router
