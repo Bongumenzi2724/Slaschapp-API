6bec0644d81507aec1c52a2791c572bd7bc635e4
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-04-20 08:06:48 +0200

    "Male,Female,Rather not say"

--- models/UserRegistrationSchema.js
+++ models/UserRegistrationSchema.js
@@ -79,7 +79,7 @@ const UserSchema = new mongoose.Schema({
     },
     gender:{
         type:String,
-        enum:['male','female','other'],
+        enum:['Male','Female','Rather not say'],
         default:'male'
     },
     interests:{
