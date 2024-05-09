d4f59e85444b17a06779dbf1c69a15d7ead0d0f8
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-05-09 13:17:04 +0200

    "Auction URLs"

--- routes/bait_plant.js
+++ routes/bait_plant.js
@@ -1,10 +1,10 @@
 const express=require('express')
 const router=express.Router()
 const {create_bait_plant,update_bait_plant,single_bait_plant,read_bait_plants,delete_bait_plant}=require('../controllers/bait_plant_controllers');
-router.post('/create',create_bait_plant);
-router.get('/single/:id',single_bait_plant)
-router.get('/all',read_bait_plants);
-router.delete('/delete/:id',delete_bait_plant);
-router.patch('/update/:id',update_bait_plant);
+router.post('/auction/:auctionID/bait/:baitID',create_bait_plant);
+router.get('/auction/:auctionID/bait/:baitID',single_bait_plant)
+router.get('/auction/:auctionID/bait',read_bait_plants);
+router.delete('/auction/:auctionID/bait/:baitID',delete_bait_plant);
+router.patch('/auction/:auctionID/bait/:baitID',update_bait_plant);
 
 module.exports=router
