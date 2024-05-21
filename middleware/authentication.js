ecae9ca00ef1f114bb1727ebf9ba1ef02bf3cfc5
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-05-21 15:46:56 +0200

    "Authentication Update"

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -13,7 +13,7 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, process.env.JWT_SECRET)
     //console.log(payload);
-    console.log(payload.userId)
+    //console.log(payload.userId)
    
     //console.log(payload.name);
     // attach the user to the business routes
