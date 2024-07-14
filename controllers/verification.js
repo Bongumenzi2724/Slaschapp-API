597470e1de3935d32e18c7cef3ce17bd495201f0
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-07-14 03:43:05 +0200

    "Latest Commit"

--- controllers/verification.js
+++ controllers/verification.js
@@ -4,23 +4,19 @@ const Auction=require('../models/AuctionSchema');
 const auctionVerification=async(req,res)=>{
     try {
         const{auctionID,userId}=req.body;
-
-        console.log(req.body);
-
         //search for the auction
         const auction=await Auction.findOne({_id:auctionID});
-
-        //const auctionID=(auction._id).toString();
         if(!auction){
             throw new NotFoundError(`No auction with id ${req.params.auctionId}`)
         }
-        //const auctionID=(auction._id).toString();
+        console.log(req.user.userId);
         if(req.user.userId===userId && auctionID===(auction._id).toString()){
             return res.status(200).json({message:"Authorized to checkout",status:true});
         }else{
             return res.status(404).json({message:"Not Authorized To checkout",status:false});
         }
     } catch (error) {
+        console.log(error);
         return res.status(500).json({message:error.message})
     }
 }
--- routes/cart_routes.js
+++ routes/cart_routes.js
@@ -3,10 +3,17 @@ const router=require('express').Router()
 const {create_cart,getCart,get_business_cart,update_cart, getAllOrders, searchBasedOnCode, user_orders} = require('../controllers/cart_controller');
 
 router.post('/add-bait',create_cart);
+
 router.patch('/update-cart/:cartId',update_cart);
+
 router.get('/:cartId',getCart);
+
 router.get('/orders/:userId',getAllOrders);
+
 router.get('/codes/order/:code',searchBasedOnCode);
-router.get('/orders/:auctionId',get_business_cart)
-router.get('/owner/orders',user_orders)
+
+router.get('/orders/:auctionId',get_business_cart);
+
+router.get('/owner/orders',user_orders);
+
 module.exports=router
