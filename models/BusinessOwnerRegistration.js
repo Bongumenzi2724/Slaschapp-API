24624b8f5366522765fc37c4277a49cb0ebf6d6c
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-07-17 11:12:52 +0200

    "Last Commit"

--- models/BusinessOwnerRegistration.js
+++ models/BusinessOwnerRegistration.js
@@ -93,6 +93,7 @@ const BusinessOwnerRegistrationSchema = new mongoose.Schema({
 BusinessOwnerRegistrationSchema.pre('save',async function(){
     //const salt = await bcrypt.genSalt(10);
     //this.password = await bcrypt.hash(this.password,salt)
+    
 })
 
 BusinessOwnerRegistrationSchema.methods.createJWT=function(){
