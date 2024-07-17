78c7500adfa47ec009c4ad27ace4eff4aa3e3786
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-07-17 15:28:58 +0200

    "Latest Commit"

--- controllers/verification.js
+++ controllers/verification.js
@@ -4,13 +4,14 @@ const Auction=require('../models/AuctionSchema');
 const auctionVerification=async(req,res)=>{
     try {
         const{auctionID,userId}=req.body;
-        
+
         const auction=await Auction.findOne({_id:auctionID});
         if(!auction){
             throw new NotFoundError(`No auction with id ${req.params.auctionId}`)
         }
-
-        if(req.user.userId===userId){
+        const userID=(req.user.userId).toString();
+        const userID2=userId.toString();
+        if(userID===userID2){
             return res.status(200).json({message:"Authorized to checkout",status:true});
         }else{
             return res.status(404).json({message:"Not Authorized To checkout",status:false});
