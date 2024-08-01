d80204bc058aac95c68d33f2ce1f1a1dd6852fe6
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-08-01 15:36:43 +0200

    "Latest Commit"

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -31,8 +31,8 @@ const sendEmail=async(email,resetToken)=>{
 
 
     } catch (error) {
-        console.error(error);
-        return res.status(500).json({message:error.message});
+        //console.error(error);
+        //return res.status(500).json({message:error.message});
     }
 }
 
