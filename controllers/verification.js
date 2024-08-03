5518b14af705ee303087fced3398e525f01f5d50
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-08-03 18:04:12 +0200

    "Latest Commit"

--- controllers/verification.js
+++ controllers/verification.js
@@ -2,6 +2,7 @@ const { NotFoundError } = require('../errors');
 const Auction=require('../models/AuctionSchema');
 const sendEmail = require('../utils/sendEmail');
 const generateOTP = require('../utils/generateOtp');
+const BusinessOwner=require('../models/BusinessOwnerRegistration');
 const auctionVerification=async(req,res)=>{
 
     try {
@@ -15,22 +16,19 @@ const auctionVerification=async(req,res)=>{
 
         }
         
-        const userID=(req.user.userId).toString();
+        const auction_owner_id=(auction.createdBy).toString();
         
-        const userID2=userId.toString();
+        console.log(auction_owner_id);
 
-        const userToCheckOut=await User.findById({_id:userID});
-        console.log(userID===userID2);
+        console.log(auction_owner_id===userId);
 
-        if(userID===userID2){
+        if(auction_owner_id===userId){
             const otp=generateOTP();
-
             await sendEmail(userToCheckOut.email,otp);
             //wait for the user to respond in time
             //make time interval to insert the otp
             //if the time expires deny checkout, set the status to false and return the appropriate message
 
-            //
             console.log("Successful Checkout");
 
             return res.status(200).json({message:"Authorized to checkout",status:true});
