5c25e0098bd887a672bb23535f017024ec07bd40
Bongumenzi2724 <bongumenzinzama@gmail.com>
2024-08-03 18:38:42 +0200

    "Latest Commit"

--- controllers/verification.js
+++ controllers/verification.js
@@ -5,23 +5,15 @@ const BusinessOwner=require('../models/BusinessOwnerRegistration');
 const auctionVerification=async(req,res)=>{
 
     try {
-
         const{auctionID,userId}=req.body;
-
         const auction=await Auction.findOne({_id:auctionID});
-
         if(!auction){
             throw new NotFoundError(`No auction with id ${auctionID}`);
         }
         const auction_owner_id=(auction.createdBy).toString();
-        console.log("True Or False");
-        console.log(auction_owner_id===userId);
-
         if(auction_owner_id===userId){
-            console.log("Successful Checkout");
             return res.status(200).json({message:"Authorized to checkout",status:true});
         }else{
-            console.log("Unsuccessful Checkout");
             return res.status(404).json({message:"Not Authorized To checkout",status:false});
         }
     } catch (error) {
